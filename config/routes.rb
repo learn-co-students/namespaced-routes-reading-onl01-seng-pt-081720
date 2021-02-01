eedb09f861f0550b555d2cef45f33dc0bb8ba50a
tonitdiep <adiep123@gmail.com>
2021-01-31 22:33:58 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index show new create edit update]
 
   # get '/admin/stats', to: 'stats#index'   #preferred for only admin to manage stats
+  
   # get '/stats', to: 'stats#index'
   #too mess so make it into a scope
+  
   # get '/admin/authors/new', to: 'authors#new'
   # get '/admin/authors/delete', to: 'authors#delete'
   # get '/admin/authors/create', to: 'authors#create'
