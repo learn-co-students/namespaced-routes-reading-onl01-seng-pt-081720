6a72764c9c16b148aaa46178c1a06dda43a6d464
tonitdiep <adiep123@gmail.com>
2021-01-31 21:39:06 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  # get '/admin/stats', to: 'stats#index'   #preferred for only admin to manage stats
+  # get '/stats', to: 'stats#index'
+  #too mess so make it into a scope
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'authors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
 
+  namespace :admin do
+  # scope '/admin', module: 'admin' do
+    resources :stats, only: [:index]
+  end
   root 'posts#index'
 end
